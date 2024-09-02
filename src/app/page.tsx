0a241aba52bad2d0d46590e0d33f9c19720d7471
GilvanPC <you@example.com>
2024-09-02 09:55:12 -0300

    add sponsorship-logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,11 +33,15 @@ export default function Home() {
         <div className={styles.sponsors}>
           <p className={styles.sponsorsTitle}> Quem apoia a nossa missão </p> 
           <div className={styles.sponsorsLogoContainer}>
+            <Image src="/logos/ita.png" alt="ita"  sizes="100vw" width={0} height={0}  className={styles.sponsorsLogo}/>
+            <Image src="/patrocinio/feng.jpg" alt="feng"  sizes="100vw" width={0} height={0}  className={styles.sponsorsLogo}/>
+            <Image src="/patrocinio/lababerto.jpg" alt="feng"  sizes="100vw" width={0} height={0}  className={styles.sponsorsLogo}/>
             <Image src="/patrocinio/itaex.png" alt="itaex" sizes="100vw" width={0} height={0} className={styles.sponsorsLogo}/>
             <Image src="/patrocinio/onshape.png" alt="onshape"   sizes="100vw" width={0} height={0}   className={styles.sponsorsLogo}/>
             <Image src="/patrocinio/simscale.svg" alt="simscale"  sizes="100vw" width={0} height={0}  className={styles.sponsorsLogo}/>
-            <Image src="/patrocinio/feng.jpg" alt="feng"  sizes="100vw" width={0} height={0}  className={styles.sponsorsLogo}/>
-            <Image src="/logos/ita.png" alt="ita"  sizes="100vw" width={0} height={0}  className={styles.sponsorsLogo}/>
+            <Image src="/patrocinio/tmotor.png" alt="simscale"  sizes="100vw" width={0} height={0}  className={styles.sponsorsLogo}/>
+            <Image src="/patrocinio/scorpion.png" alt="simscale"  sizes="100vw" width={0} height={0}  className={styles.sponsorsLogo}/>
+            <Image src="/patrocinio/carbonstore.png" alt="simscale"  sizes="100vw" width={0} height={0}  className={styles.sponsorsLogo}/>
           </div>
           <Button onClick={() => router.push('/patrocinio')} style={{backgroundColor:'var(--secondary-yellow)',fontSize:'1.5vw'}}>
             Patrocinadores
